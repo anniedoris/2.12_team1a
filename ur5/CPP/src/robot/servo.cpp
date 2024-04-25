ce021a7a29071d885785b53396664d35b9a7690b
michaeldonnellan2 <michaeldonnellan2@gmail.com>
2024-04-25 12:58:47 -0400

    editing the servo code

--- ur5/CPP/src/robot/servo.cpp
+++ ur5/CPP/src/robot/servo.cpp
@@ -8,12 +8,13 @@ Servo servo1;
 Servo servo2;
 
 // Define min and max positions for the servos
-const int SERVO1_MIN = 75; // Minimum position for servo1
-const int SERVO1_MAX = 115; // Maximum position for servo1
-const int SERVO2_MIN = 75; // Minimum position for servo2
-const int SERVO2_MAX = 115; // Maximum position for servo2
+const int SERVO1_MIN = 45; // Minimum position for servo1
+const int SERVO1_MAX = 110; // Maximum position for servo1
+const int SERVO2_MIN = 45; // Minimum position for servo2
+const int SERVO2_MAX = 130; // Maximum position for servo2
 
-int pos = 90; // Initial position for both servos (middle)
+int pos = 90; // Initial position for servos
+int position2;
 
 void setup() {
   // Attach servos to digital pins on the Arduino
@@ -26,17 +27,22 @@ void setup() {
 
 void loop() {
   // Check if data is available to read
+  position2 = analogRead(6);
+  Serial.println(position2);
   if (Serial.available() > 0) {
     char input = Serial.read(); // Read the incoming byte
 
     if (input == 'o') { // If 'i' is pressed, move inward
       pos = max(pos - 1, SERVO1_MIN); // Decrease position but not less than SERVO1_MIN
+      //Serial.println(pos);
     } else if (input == 'i') { // If 'o' is pressed, move outward
       pos = min(pos + 1, SERVO1_MAX); // Increase position but not more than SERVO1_MAX
+      //Serial.println(pos);
+      
     }
 
     // Move servos to new position within defined limits
     servo1.write(pos);
-    servo2.write(map(pos-90, SERVO1_MIN, SERVO1_MAX, SERVO2_MAX, SERVO2_MIN)); // Map and inverse position for the second servo
+    servo2.write(map(pos+90, SERVO1_MIN, SERVO1_MAX, SERVO2_MAX, SERVO2_MIN)); // Map and inverse position for the second servo
   }
 }
