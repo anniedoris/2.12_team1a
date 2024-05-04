8dcdd9fe28cc0c59fb7250cb0d3eb394b28d3447
michaeldonnellan2 <michaeldonnellan2@gmail.com>
2024-05-04 16:30:47 -0400

    Revised the test_5_1.cpp file. This is the most up-to-date file that controls the electromagnet and the servos on the gripper.

--- ur5/CPP/src/robot/test_5_1.cpp
+++ ur5/CPP/src/robot/test_5_1.cpp
@@ -1,12 +1,18 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
+#include "MotorDriver.h"
 
 // Adapted from https://github.com/madhephaestus/ESP32Servo/blob/master/examples/Multiple-Servo-Example-Arduino/Multiple-Servo-Example-Arduino.ino
-// Updated on 5/1/24 - works 
+// Updated on 5/4/24 - works 
 
 // Create two servo objects
 Servo servo1;
 Servo servo2;
+
+// Pins connected to the Cytron Motor Driver
+const int DIRPin = 39; // connected to DIR1 pin on Cytron
+const int PWMPin = 41; // connected to PWM1 pin on Cytron
+MotorDriver motor(DIRPin, PWMPin, 5); 
 
 int pos; // Initial position for servos
 int pos2;
@@ -17,6 +23,8 @@ int IN_POS_RIGHT = OUT_POS_RIGHT + GRASP_DELTA;
 bool move_in = true;
 int IN_POS_LEFT = 0;
 int OUT_POS_LEFT = IN_POS_LEFT + GRASP_DELTA;
+int MID_POS_RIGHT = (OUT_POS_RIGHT + GRASP_DELTA) / 2;
+int MID_POS_LEFT = (IN_POS_LEFT + GRASP_DELTA) / 2;
 
 int micro_sec;
 int read_val;
@@ -33,6 +41,7 @@ void setup() {
   servo2.attach(6, 500, 2500);
   // servo1.attach(9); // Servo 1 on pin 9
   // servo2.attach(6); // Servo 2 on pin 6
+  motor.setup();
 
   // Initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
@@ -49,21 +58,39 @@ void loop() {
     char input = Serial.read(); // Read the incoming byte
 
     if (input == 'o') 
-    { // If 'i' is pressed, move inward
+    { // If 'o' is pressed, move outward
       // pos = OUT_POS_RIGHT; // Decrease position but not less than SERVO1_MIN
-      //Serial.println(pos);
+      Serial.println("Servo Position: Out");
       servo1.write(OUT_POS_RIGHT);
       servo2.write(OUT_POS_LEFT);
     } 
     
     else if (input == 'i') 
-    { // If 'o' is pressed, move outward
+    { // If 'i' is pressed, move inward
       // pos = IN_POS_RIGHT; // Increase position but not more than SERVO1_MAX
-      //Serial.println(pos);
+      Serial.println("Servo Position: In");
       servo1.write(IN_POS_RIGHT);
       servo2.write(IN_POS_LEFT);
       
     }
+
+     else if (input == 'm') 
+    { // If 'm' is pressed, move to middle
+      // pos = MID_POS_RIGHT; // Increase position but not more than SERVO1_MAX
+      Serial.println("Servo Position: Middle");
+      servo1.write(MID_POS_RIGHT);
+      servo2.write(MID_POS_LEFT);
+      
+    }
+
+    if (input == '1') {
+      motor.drive(1.0);
+      Serial.println("Electromagnet turned on");
+    } else if (input == '0') {
+      motor.drive(0.0);
+      Serial.println("Electromagnet turned off");
+    }
   }
+}
 
   
--- ur5/CPP/src/test_code/electromagnet.cpp
+++ ur5/CPP/src/test_code/electromagnet.cpp
@@ -1,15 +1,16 @@
 // Code to control the electromagnet attached to the end effector 
 #include <Arduino.h>
+#include "MotorDriver.h"
 
 // Pins connected to the Cytron Motor Driver
-const int DIRPin = 1; // connected to DIR1 pin on Cytron
-const int PWMPin = 2; // connected to PWM1 pin on Cytron 
+const int DIRPin = 39; // connected to DIR1 pin on Cytron
+const int PWMPin = 41; // connected to PWM1 pin on Cytron
+MotorDriver motor(DIRPin, PWMPin, 0); 
 
 void setup() {
   Serial.begin(115200);
   
-  pinMode(DIRPin, OUTPUT);
-  pinMode(PWMPin, OUTPUT);
+  motor.setup();
 }
 
 void loop() {
@@ -17,12 +18,10 @@ void loop() {
     char command = Serial.read();
     
     if (command == '1') {
-      digitalWrite(DIRPin, HIGH); // Turn on electromagnet
-      analogWrite(PWMPin, 255);
+      motor.drive(1.0);
       Serial.println("Electromagnet turned on");
     } else if (command == '0') {
-      digitalWrite(DIRPin, LOW); // Turn off electromagnet
-      analogWrite(PWMPin, 0);
+      motor.drive(0.0);
       Serial.println("Electromagnet turned off");
     }
   }
