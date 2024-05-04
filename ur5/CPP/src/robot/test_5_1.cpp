8abca33bca597bbd8b24ae348e0d941f8a0fecf0
michaeldonnellan2 <michaeldonnellan2@gmail.com>
2024-05-04 19:11:57 -0400

    Cleaning up the test_5_1.cpp file; adding comments.

--- ur5/CPP/src/robot/test_5_1.cpp
+++ ur5/CPP/src/robot/test_5_1.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
-#include <ESP32Servo.h>
-#include "MotorDriver.h"
+#include <ESP32Servo.h> // Required for the servos
+#include "MotorDriver.h" // Required for the Cytron motor driver that is connected to the electromagnet 
 
-// Adapted from https://github.com/madhephaestus/ESP32Servo/blob/master/examples/Multiple-Servo-Example-Arduino/Multiple-Servo-Example-Arduino.ino
-// Updated on 5/4/24 - works 
+// Updated on 5/4/24
+// This file includes code that runs the servos, electromagnet, and IR camera on the gripper.  
 
 // Create two servo objects
 Servo servo1;
@@ -12,54 +12,42 @@ Servo servo2;
 // Pins connected to the Cytron Motor Driver
 const int DIRPin = 39; // connected to DIR1 pin on Cytron
 const int PWMPin = 41; // connected to PWM1 pin on Cytron
-MotorDriver motor(DIRPin, PWMPin, 5); 
+MotorDriver motor(DIRPin, PWMPin, 5); // channel 5
 
 int pos; // Initial position for servos
-int pos2;
-int position2;
 int GRASP_DELTA = 70;
 int OUT_POS_RIGHT = 0;
 int IN_POS_RIGHT = OUT_POS_RIGHT + GRASP_DELTA;
-bool move_in = true;
 int IN_POS_LEFT = 0;
 int OUT_POS_LEFT = IN_POS_LEFT + GRASP_DELTA;
 int MID_POS_RIGHT = (OUT_POS_RIGHT + GRASP_DELTA) / 2;
 int MID_POS_LEFT = (IN_POS_LEFT + GRASP_DELTA) / 2;
 
-int micro_sec;
-int read_val;
-
 void setup() {
-  // Attach servos to digital pins on the Arduino
-  ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
-	servo1.setPeriodHertz(50);    // standard 50 hz servo
-	servo1.attach(9, 500, 2500); // attaches the servo on pin 18 to the servo object
-  servo2.setPeriodHertz(50);
-  servo2.attach(6, 500, 2500);
-  // servo1.attach(9); // Servo 1 on pin 9
-  // servo2.attach(6); // Servo 2 on pin 6
-  motor.setup();
+  // Attach servos to digital pins on the Arduino and assign channels
+  ESP32PWM::allocateTimer(0); // channel 0
+	ESP32PWM::allocateTimer(1); // channel 1
+	ESP32PWM::allocateTimer(2); // channel 2
+	ESP32PWM::allocateTimer(3); // channel 3
+	servo1.setPeriodHertz(50); // standard 50 hz servo
+	servo1.attach(9, 500, 2500); // Servo 1 on pin 9
+  servo2.setPeriodHertz(50); // standard 50 hz servo
+  servo2.attach(6, 500, 2500); // Servo 2 on pin 6
 
-  // Initialize serial communication at 115200 bits per second:
-  Serial.begin(115200);
-  servo1.write(OUT_POS_RIGHT);
-  servo2.write(OUT_POS_LEFT);
+  motor.setup(); // Set up the electromagnet
   
+  Serial.begin(115200); // Initialize serial communication at 115200 bits per second
+  servo1.write(OUT_POS_RIGHT); // Move servo1 to its initial position
+  servo2.write(OUT_POS_LEFT); // Move servo2 to its initial position
 }
 
 void loop() {
-
-  // GOOD CODE
   if (Serial.available() > 0) 
   {
     char input = Serial.read(); // Read the incoming byte
 
     if (input == 'o') 
     { // If 'o' is pressed, move outward
-      // pos = OUT_POS_RIGHT; // Decrease position but not less than SERVO1_MIN
       Serial.println("Servo Position: Out");
       servo1.write(OUT_POS_RIGHT);
       servo2.write(OUT_POS_LEFT);
@@ -67,20 +55,16 @@ void loop() {
     
     else if (input == 'i') 
     { // If 'i' is pressed, move inward
-      // pos = IN_POS_RIGHT; // Increase position but not more than SERVO1_MAX
       Serial.println("Servo Position: In");
       servo1.write(IN_POS_RIGHT);
       servo2.write(IN_POS_LEFT);
-      
     }
 
      else if (input == 'm') 
     { // If 'm' is pressed, move to middle
-      // pos = MID_POS_RIGHT; // Increase position but not more than SERVO1_MAX
       Serial.println("Servo Position: Middle");
       servo1.write(MID_POS_RIGHT);
       servo2.write(MID_POS_LEFT);
-      
     }
 
     if (input == '1') {
